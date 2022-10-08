835f29faed33878c241286c33056b9f31c1b7811
Nazmulhudashawon <shawonnazmulhuda@gmail.com>
2022-10-08 14:21:39 +0600

    first commit

---
+++ src/helpers/Questions.js
@@ -0,0 +1,35 @@
+export const Questions = [
+  
+  {
+    prompt: "Which of this is not a programming language?",
+    optionA: "Python",
+    optionB: "JavaScript",
+    optionC: "MC-03",
+    optionD: "Java",
+    answer: "optionC",
+  },
+  {
+    prompt: "Which of this is not a javascript framework?",
+    optionA: "React",
+    optionB: "Angular",
+    optionC: "Vue",
+    optionD: "Java",
+    answer: "optionD",
+  },
+  {
+    prompt: "How do you create a function in JavaScript??",
+    optionA: "function=myfunction()",
+    optionB: "function myfunction()",
+    optionC: "function-myfunction()",
+    optionD: "function:myfunction()",
+    answer: "optionB",
+  },
+  {
+    prompt: "What is the correct command to create a new React project??",
+    optionA: "npx-create-react-app",
+    optionB: "npm-create-react-app",
+    optionC: "npm-create-react-app myapp",
+    optionD: "npx-create-react-app myapp",
+    answer: "optionD",
+  }
+];
